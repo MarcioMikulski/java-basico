10b500d88618f232ba2a7dff0ed3bdf2aed9b01b
Marcio Mikulski <mugemikulski@gmail.com>
2024-05-17 20:42:21 -0300

    Desafio-DIO

---
+++ ContaBanco/src/ContaBanco/ContaTerminal.java
@@ -0,0 +1,33 @@
+package ContaBanco;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = sc.nextLine();
+        
+        System.out.println("Por favor digite o número da sua conta :");
+        int numero = sc.nextInt();
+        
+        
+        System.out.println("Por favor digite o seu nome: ");
+        sc.nextLine();
+        String nomeCliente = sc.nextLine();
+        
+        System.out.println("Por favor Digite o seu saldo ");
+        double saldo = sc.nextDouble();
+        System.out.println("-------------------------------------------");
+        
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco. Sua agência é %s, conta %d, e seu saldo %.2f já está disponível para saque.%n", 
+                nomeCliente, agencia, numero, saldo);
+
+sc.close();
+	}
+
+}

0daecf2c51894fb2dd5534e41fb2991aeb3e35fa
Marcio Mikulski <mugemikulski@gmail.com>
2024-05-17 19:43:58 -0300

    SmartTv

---
+++ sistemaSmartTv/src/sistemaSmartTv/Program.java
@@ -0,0 +1,32 @@
+package sistemaSmartTv;
+
+public class Program {
+
+	public static void main(String[] args) {
+		SmartTv smartTv = new SmartTv();
+		
+		System.out.println("TV Ligada ? " + smartTv.ligada);
+		System.out.println("Canal atual : " + smartTv.canal);
+		System.out.println("Volume atual : " + smartTv.volume);
+		
+		smartTv.ligar();
+		System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+		
+		smartTv.desligar();
+		System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+		
+		smartTv.aumentarVolume();
+		System.out.println("Volume atual : " + smartTv.volume);
+		
+		smartTv.diminuirVolume();
+		System.out.println("Volume atual : " + smartTv.volume);
+		
+		smartTv.avancarCanal();
+		System.out.println("Canal atual : " + smartTv.canal);
+		smartTv.voltarCanal();
+		System.out.println("Canal atual : " + smartTv.canal);
+		
+		
+	}
+
+}
